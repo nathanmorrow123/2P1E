651eb7b8586210f7f3d6188f9c7ed050c9dbc0e9
Nathan Morrow <nathanmorrow123@gmail.com>
2024-08-07 15:06:36 -0400

    Added more files

--- Barrier Curve (Fast Evader)/BarrierCurveSolver.py
+++ Barrier Curve (Fast Evader)/BarrierCurveSolver.py
@@ -28,34 +28,105 @@ z0 = 1/np.sqrt(2)  # Initial z value
 x_end = 1.5  # Ending x value
 h = 0.00001  # Step size
 
+
 # Perform the numerical integration using RK4
 x, z = runge_kutta_4(dz_dx, x0, z0, x_end, h)
-x1 = np.linspace(0,x0,50)
-y1 = np.linspace(0,z0,50)
+
+#Finding the last x position in solution
 nan_where = np.argwhere(np.isnan(z))
 first_nan = nan_where[0]
 print(x[first_nan-1])
 print(z[first_nan-1])
 x = x[0:int(first_nan-1)]
 z = z[0:int(first_nan-1)]
+
+x_elip = [[],[],[]]
+z_elip = [[],[],[]]
+i = 0
+x0_arr = [x0-0.1,x0,x0+0.1]
+# Finding the ellipse lines
+for x0 in x0_arr:
+    for theta in np.linspace(np.pi/2,np.pi,1000):
+        xe = (x0+np.sqrt(1-z0)) + np.cos(theta)
+        ze = np.sin(theta)
+        if ((xe <= np.sqrt(2) and ze <= 1 ) and (np.abs(ze/xe) <= 1) and (xe >= 1/np.sqrt(2))):
+            x_elip[i].append(xe)
+            z_elip[i].append(ze)
+        
+    i += 1
+
+x0 = 1/np.sqrt(2)  # Initial x value
+z0 = 1/np.sqrt(2)  # Initial z value
+print(x_elip)
+print(z_elip)     
+
+
+# Finite Boundry lines
+x1 = np.linspace(0,x0,50)
+z1 = np.linspace(0,z0,50)
+x2 = np.linspace(x0,1,50)
+z2 = np.linspace(z0,1,50)
+x3 = np.linspace(1,x[-1],50)
+z3 = np.ones(50)
+
 # Perform polynomial regression to find the best-fit equation
-degree = 2  # Degree of the polynomial
+degree = 3  # Degree of the polynomial
 coeffs = np.polyfit(x, z, degree)
 p = np.poly1d(coeffs)
 print(coeffs)
 
+def add_arrow(line, position=None, direction='right', size=15, color=None):
+    """
+    add an arrow to a line.
+
+    line:       Line2D object
+    position:   x-position of the arrow. If None, mean of xdata is taken
+    direction:  'left' or 'right'
+    size:       size of the arrow in fontsize points
+    color:      if None, line color is taken.
+    """
+    line = line[0]
+    color = line.get_color()
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+
+    if position is None:
+        position = ydata.mean()
+    # find closest index
+    start_ind = int(len(xdata)/2)
+    if direction == 'right':
+        end_ind = start_ind + 1
+    else:
+        end_ind = start_ind - 1
+
+    line.axes.annotate('',
+        xytext=(xdata[start_ind], ydata[start_ind]),
+        xy=(xdata[end_ind], ydata[end_ind]),
+        arrowprops=dict(arrowstyle="->", color=color),
+        size=size
+    )
+
+
 # Plot the results
 #plt.style.use('dark_background')
 plt.rcParams["font.family"] = "Times New Roman"
-plt.plot(x, z, label='Numerical Solution (RK4)')
-#plt.plot(x, p(x), label=f'Best Fit Polynomial (degree {degree})')
-plt.plot(x1,y1)
+plt.set_cmap('cool')
+line = plt.plot(x, z, label='Numerical Solution (RK4)')
+add_arrow(line, direction='left')
+
+for i in [0,1,2]:
+    line = plt.plot(x_elip[i],z_elip[i], label = f'Circular arc with $x_0$  = {np.round(x0_arr[i],4)}')
+    add_arrow(line)
+plt.plot(x1,z1, color = 'black')
+plt.plot(x2,z2, color = 'black')
+plt.plot(x3,z3, color = 'black')
+
 plt.xlabel('$x_P$')
 plt.xticks([0,1/np.sqrt(2),1, x[-1]])
 plt.yticks([0,1/np.sqrt(2),1])
 plt.gca().set_aspect('equal')
 plt.ylabel('$z$')
-plt.title('Numerical Solution of the Barrier Curve')
+plt.title('Sub-Optimal Flow Field, $\mu = \sqrt{2}$')
 plt.legend()
 plt.grid(True)
 plt.savefig('Barrier_Curve.pdf')
---
+++ Optimal Control/optimal_control.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+mu = np.sqrt(2)  # (V_e / V_p)
+T = 5  # Total time
+dt = 0.01  # Time step
+n_steps = int(T / np.abs(dt))
+n_lines = 3
+
+# Initial conditions for reduced state space variables
+x_p0 = np.linspace(np.sqrt((mu**2 - 1) / mu), 1, n_lines, endpoint=False)
+z_0 = x_p0
+
+# Terminal conditions for costates
+lambda_x_p_0 = -1 / (x_p0 - np.sqrt(mu**2 - 1) * np.sqrt(1 - x_p0))
+lambda_z_0 = 1 / (x_p0 - np.sqrt(mu**2 - 1) * np.sqrt(1 - x_p0))
+
+# Arrays to store results
+x_p_arr = np.zeros((n_steps + 1, len(x_p0)))
+z_arr = np.zeros((n_steps + 1, len(z_0)))
+x_p_arr[0] = x_p0
+z_arr[0] = z_0
+
+# Forward integration of state equations
+for i in range(len(x_p0)):
+    # Assign initial variables
+    x_p = x_p0[i]
+    z = z_0[i]
+    lambda_x_p = lambda_x_p_0[i]
+    lambda_z = lambda_z_0[i]
+    
+    for t in range(n_steps):
+        # State equations
+        x_p_dot = (1 / 2) * (z - lambda_x_p / np.sqrt(lambda_x_p**2 + (1 - z**2) / x_p * lambda_z**2))
+        z_dot = (mu**2 - 1) * np.sqrt(1 - z**2) + (1 / 2) * ((1 - z**2) / x_p**2) * (x_p + lambda_z / np.sqrt(lambda_z**2 * (1 - z**2) / x_p**2 + lambda_x_p**2))
+
+        x_p += x_p_dot * dt
+        z += z_dot * dt
+
+        x_p_arr[t + 1, i] = x_p
+        z_arr[t + 1, i] = z
+
+        # Reverse integration of costate equations
+        lambda_x_p_dot = (1 / 2) * lambda_z * (1 - z**2) / x_p**3 * (lambda_z / np.sqrt(lambda_x_p**2 + (1 - z**2) / x_p**2 * lambda_z**2) - x_p)
+        lambda_z_dot = (1 / 2) * ((lambda_z**2 / x_p**2 * z) / np.sqrt(lambda_x_p**2 + (1 - z**2) / x_p**2 * lambda_z**2)) + (1 / 2) * lambda_x_p - z * lambda_z / x_p - np.sqrt(mu**2 - 1) * z / (1 - z**2) * lambda_z
+
+        lambda_x_p += lambda_x_p_dot * dt
+        lambda_z += lambda_z_dot * dt
+
+# Finite boundary lines
+x0 = np.sqrt(mu**2 - 1) / mu  # Initial x value
+z0 = np.sqrt(mu**2 - 1) / mu  # Initial z value
+x1 = np.linspace(0, x0, 50)
+z1 = np.linspace(0, z0, 50)
+x2 = np.linspace(x0, 1, 50)
+z2 = np.linspace(z0, 1, 50)
+x3 = np.linspace(1, 1.454, 50)
+z3 = np.ones(50)
+
+def add_arrow(line, position=None, direction='right', size=15, color=None):
+    """
+    Add an arrow to a line.
+
+    line:       Line2D object
+    position:   x-position of the arrow. If None, mean of xdata is taken
+    direction:  'left' or 'right'
+    size:       size of the arrow in fontsize points
+    color:      if None, line color is taken.
+    """
+    line = line[0]
+    color = line.get_color()
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+    
+    if position is None:
+        position = ydata.mean()
+    
+    # find closest index
+    start_ind = int(len(xdata) / 2)
+    if direction == 'right':
+        end_ind = start_ind + 1
+    else:
+        end_ind = start_ind - 1
+    
+    line.axes.annotate(
+        '',
+        xytext=(xdata[start_ind], ydata[start_ind]),
+        xy=(xdata[end_ind], ydata[end_ind]),
+        arrowprops=dict(arrowstyle="->", color=color),
+        size=size
+    )
+
+# Plotting the results
+plt.rcParams["font.family"] = "Times New Roman"
+plt.set_cmap('cool')
+for i in range(n_lines):
+    line = plt.plot(x_p_arr[:, i], z_arr[:, i])
+    add_arrow(line, direction='left')
+plt.plot(x1, z1, color='black')
+plt.plot(x2, z2, color='black')
+plt.plot(x3, z3, color='black')
+plt.xticks([0, 1 / np.sqrt(2), 1, 1.454])
+plt.yticks([0, 1 / np.sqrt(2), 1])
+plt.axis('equal')
+plt.xlabel('$x_p$')
+plt.ylabel('$z$')
+plt.title('Optimal Control Flow Field')
+plt.grid()
+plt.savefig('Optimal_Control_Flow_Field.pdf')
+plt.show()
