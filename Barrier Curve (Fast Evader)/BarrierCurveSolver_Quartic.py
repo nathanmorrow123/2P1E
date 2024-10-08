4153e83f2331e5eb4d6816d6b365c54333269a3e
root <root@e-bgbgbfbg-vjs9k-0.e-bgbgbfbg-vjs9k.ace-hub.svc.cluster.local>
2024-10-08 14:36:23 +0000

    Added some more

--- Barrier Curve (Fast Evader)/BarrierCurveSolver_Quartic.py
+++ Barrier Curve (Fast Evader)/BarrierCurveSolver_Quartic.py
@@ -32,19 +32,19 @@ def boundryCheck(t,x_E,y_E):
     a0 =  x_E**2 - y_E**2 - 0.21
     res = a3*t**3+a2*t**2+a1*t+a0
 
-    return np.isclose(0,res)
+    return res
 
 def solve_quartic(x_P, x_E, last_y_E=None):
     if last_y_E is not None:
         if x_E < (x_P - 1):
-            y_values = np.linspace(last_y_E, 1, 10000)  # Scan y_E from last highest to 1.0 
+            y_values = np.linspace(last_y_E, 1, 1000)  # Scan y_E from last highest to 1.0 
         else:
-            y_values = np.linspace(np.sqrt(1 - (x_P - x_E)**2), 1, 10000)  # Scan y_E from -1.0 to 1.0
+            y_values = np.linspace(np.sqrt(1 - (x_P - x_E)**2), 1, 1000)  # Scan y_E from -1.0 to 1.0
     else:
         if x_E < (x_P - 1):
             y_values = np.linspace(0, 1, 10)  # Scan y_E from -1.0 to 1.0 
         else:
-            y_values = np.linspace(np.sqrt(1 - (x_P - x_E)**2), 1, 10000)  # Scan y_E from -1.0 to 1.0
+            y_values = np.linspace(np.sqrt(1 - (x_P - x_E)**2), 1, 1000)  # Scan y_E from -1.0 to 1.0
     
     max_y_E = -np.inf  # Initialize with negative infinity
 
@@ -55,35 +55,38 @@ def solve_quartic(x_P, x_E, last_y_E=None):
         real_roots = [r for r in roots if np.isreal(r) and np.real(r) >= 0 and np.imag(r) == 0 and np.isclose(p(r), 0)]
         if len(real_roots) == 4:
             max_y_E = y_E
-            print(boundryCheck(np.min(real_roots),x_E,y_E))
+            test_val = boundryCheck(np.min(real_roots),x_E,y_E)
 
     if max_y_E == -np.inf:
-        return None
+        return None, None
         
-    return max_y_E
+    return max_y_E, test_val
 
 # Function to compute and plot the barrier curve for each x_P
 def plot_barrier_curve(x_P):
-    #x_E_values = np.linspace(0, x_P, 500)
-    x_E_values = np.linspace(0,x_P,1000)
+    x_E_values = np.linspace(0,x_P,500)
     highest_y_E_values = []
+    test_vals = []
     max_y_E = None
     for x_E in x_E_values:
-        max_y_E = solve_quartic(x_P, x_E, max_y_E)
+        max_y_E,test_val = solve_quartic(x_P, x_E, max_y_E)
         if max_y_E is not None:
             highest_y_E_values.append(max_y_E)
+            test_vals.append(test_val)
         else:
             highest_y_E_values.append(np.nan)
+            test_vals.append(np.nan)
 
     plt.figure(figsize=(10, 10))
     highest_y_E_values = np.array(highest_y_E_values) # For mirroring
-
+    test_vals = np.array(test_vals)
     # Plot the original curve
     plt.scatter(x_E_values, highest_y_E_values, s=2, color='red')
     plt.scatter(-x_E_values, highest_y_E_values, s=2, color='red')
     plt.scatter(-x_E_values, -highest_y_E_values, s=2, color='red')
     plt.scatter(x_E_values, -highest_y_E_values, s=2, color='red')
-
+    plt.scatter(x_E_values, test_vals, s = 2, color = 'blue' )
+    plt.scatter(-x_E_values, test_vals, s = 2, color = 'blue')
     plt.scatter([x_P], [0], color='blue')
     capture_radius_1 = plt.Circle((x_P, 0), 1, color='blue', fill=False)
     plt.gca().add_patch(capture_radius_1)
@@ -96,7 +99,7 @@ def plot_barrier_curve(x_P):
     plt.axvline(0, color='black', linewidth=1)
     plt.xlabel('$x_E$')
     plt.ylabel('$y_E$')
-    plt.title(f'Barrier Curve 2P1E for $x_P$ = {x_P:.3f}')
+    plt.title(f'Barrier Curve 2P1E for $x_P$ = {x_P:.4f}')
     plt.grid(True)
     plt.gca().set_aspect('equal', adjustable='box')
 
@@ -129,24 +132,31 @@ def create_animation(image_files):
     ani = animation.FuncAnimation(fig, update_frame, frames=len(image_files), repeat=True)
     
     # Save the animation as mp4
-    ani.save('barrier_curve_animation.gif', dpi = 300, fps=1)
+    ani.save('barrier_curve_animation.gif', dpi = 300, fps=30)
 
 # Main function to parallelize the generation of frames
 def main():
-    #x_P_values = np.linspace(1/np.sqrt(2), 1.5, 240)  
-    x_P_values = np.array([1.1,1.2])
-    save_path = "frames"
+
+    create_frames = False
+    if create_frames:
+        x_P_values = np.linspace(1/np.sqrt(2), 1.5, 240)  
+        #x_P_values = np.array([1.1,1.2])
+        save_path = "frames"
+        
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        # Parallel processing for generating frames
+        with Pool(processes=cpu_count()) as pool:
+            image_files = list(tqdm(pool.imap(plot_barrier_curve, x_P_values), total=len(x_P_values)))
+        # Create the animation after all frames are generated
+        create_animation(image_files)
+    else:
+        files = os.listdir('frames/')
+        files = ['frames/' + s for s in files]
+        print(files)
+        create_animation(files)
     
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    # Parallel processing for generating frames
-    with Pool(processes=cpu_count()) as pool:
-        image_files = list(tqdm(pool.imap(plot_barrier_curve, x_P_values), total=len(x_P_values)))
-
-
-    # Create the animation after all frames are generated
-    create_animation(image_files)
 
 if __name__ == '__main__':
     main()
